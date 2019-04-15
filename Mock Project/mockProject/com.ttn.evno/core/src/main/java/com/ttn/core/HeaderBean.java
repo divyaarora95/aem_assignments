e83ac5750dbc4183d2c302d8097f9339f237a074
divyaarora95 <divya.arora@tothenew.com>
2019-04-15 15:27:44 +0530

    java code

--- Mock Project/mockProject/com.ttn.evno/core/src/main/java/com/ttn/core/HeaderBean.java
+++ Mock Project/mockProject/com.ttn.evno/core/src/main/java/com/ttn/core/HeaderBean.java
@@ -8,20 +8,5 @@ public class HeaderBean {
         this.navTitle = navTitle;
         this.navLink = navLink;
     }
-    
-    public String getNavTitle() {
-        return navTitle;
-    }
-    
-    public void setNavTitle(String navTitle) {
-        this.navTitle = navTitle;
-    }
-    
-    public String getNavLink() {
-        return navLink;
-    }
-    
-    public void setNavLink(String navLink) {
-        this.navLink = navLink;
-    }
+
 }
